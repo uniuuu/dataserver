19b9ae48c151e1de24bd040db51778fae20c5c17
Dan Stillman <dstillman@zotero.org>
2026-03-20 15:37:30 -0400

    Skip auth rate limiting on test site

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -260,18 +260,22 @@ class ApiController extends Controller {
 				}
 			}
 			if ($key) {
-				$maxKeyAuthFailures = 5;
-				$keyAuthFailureWindow = 300;
-				$ip = IPAddress::getIP();
-				$failCacheKey = "keyAuthFailures_" . $ip;
-				if (Z_Core::$MC->get($failCacheKey) > $maxKeyAuthFailures) {
-					$this->e429("Too many authentication failures");
+				if (!Z_ENV_TESTING_SITE) {
+					$maxKeyAuthFailures = 5;
+					$keyAuthFailureWindow = 300;
+					$ip = IPAddress::getIP();
+					$failCacheKey = "keyAuthFailures_" . $ip;
+					if (Z_Core::$MC->get($failCacheKey) > $maxKeyAuthFailures) {
+						$this->e429("Too many authentication failures");
+					}
 				}
 
 				$keyObj = Zotero_Keys::authenticate($key);
 				if (!$keyObj) {
-					Z_Core::$MC->add($failCacheKey, 0, $keyAuthFailureWindow);
-					Z_Core::$MC->increment($failCacheKey);
+					if (!Z_ENV_TESTING_SITE) {
+						Z_Core::$MC->add($failCacheKey, 0, $keyAuthFailureWindow);
+						Z_Core::$MC->increment($failCacheKey);
+					}
 					$this->e403('Invalid key');
 				}
 				$this->apiKey = $key;
