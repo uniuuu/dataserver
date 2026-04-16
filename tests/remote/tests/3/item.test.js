uniuuu/dataserver

commit 2300dd41293eb0453dc738c06449e20a66960289

Only update lastModifiedByUserID when dateModified changes
Don't update for changes that skip dateModified (e.g., collection
membership, trash, relations).

model/Item.inc.php
@@ -1020,7 +1020,7 @@ class Zotero_Item extends Zotero_DataObject {
 	}
 	
 	
-	public function save($userID=false) {
+	public function save($userID=false, $options=[]) {
 		if (!$this->_libraryID) {
 			trigger_error("Library ID must be set before saving", E_USER_ERROR);
 		}
@@ -1679,8 +1679,10 @@ class Zotero_Item extends Zotero_DataObject {
 				
 				$this->_serverDateModified = $timestamp;
 				
-				// Group item data
-				if ($isGroupLibrary && $userID) {
+				// Group item data -- only update lastModifiedByUserID if dateModified
+				// was updated (i.e., not for collection membership, trash, etc.)
+				if ($isGroupLibrary && $userID
+						&& empty($options['skipDateModifiedUpdate'])) {
 					$sql = "INSERT INTO groupItems VALUES (?, ?, ?)
 								ON DUPLICATE KEY UPDATE lastModifiedByUserID=?";
 					Zotero_DB::query($sql, array($this->_id, null, $userID, $userID), $shardID);
@@ -2424,7 +2426,7 @@ class Zotero_Item extends Zotero_DataObject {
 	 */
 	public function updateVersion($userID) {
 		$this->changed['version'] = true;
-		$this->save($userID);
+		$this->save($userID, ['skipDateModifiedUpdate' => true]);
 	}
 	
 	

model/Items.inc.php
@@ -701,24 +701,12 @@ class Zotero_Items {
 		}
 		
 		Zotero_DB::beginTransaction();
-		foreach ($shardItemIDs as $shardID => $itemIDs) {
-			// Group item data
-			if ($userID && isset($shardGroupItemIDs[$shardID])) {
-				$sql = "UPDATE groupItems SET lastModifiedByUserID=? "
-					. "WHERE itemID IN ("
-					. implode(',', array_fill(0, sizeOf($shardGroupItemIDs[$shardID]), '?')) . ")";
-				Zotero_DB::query(
-					$sql,
-					array_merge(array($userID), $shardGroupItemIDs[$shardID]),
-					$shardID
-				);
-			}
-		}
 		foreach ($libraryItems as $libraryID => $items) {
 			$itemIDs = array();
 			foreach ($items as $item) {
 				$itemIDs[] = $item->id;
 			}
+			$shardID = $libraryShards[$libraryID];
 			$version = Zotero_Libraries::getUpdatedVersion($libraryID);
 			$sql = "UPDATE items SET version=? WHERE itemID IN "
 				. "(" . implode(',', array_fill(0, sizeOf($itemIDs), '?')) . ")";
@@ -1906,7 +1894,9 @@ class Zotero_Items {
 			$item->dateModified = Zotero_DB::getTransactionTimestamp();
 		}
 		
-		$changed = $item->save($userID) || $changed;
+		$changed = $item->save($userID, [
+			'skipDateModifiedUpdate' => $skipDateModifiedUpdate
+		]) || $changed;
 		
 		// Additional steps that have to be performed on a saved object
 		if ($twoStage) {

tests/remote/tests/3/item.test.js
@@ -1603,6 +1603,126 @@ describe('Items', function () {
 		assert.equal(config.get('username'), json.meta.createdByUser.username);
 	});
 
+	it('should not update lastModifiedByUser for non-field changes', async function () {
+		let groupID = config.get('ownedPrivateGroupID');
+
+		// Create item as user 1
+		let json = await API.groupCreateItem(groupID, 'book', {}, 'json');
+		let key = json.key;
+		let version = json.version;
+
+		// Modify a field as user 2 so lastModifiedByUser gets set
+		API.useAPIKey(config.get('user2APIKey'));
+		let response = await API.groupPost(
+			groupID,
+			'items',
+			JSON.stringify([{
+				key: key,
+				version: version,
+				title: 'Modified Title'
+			}]),
+			['Content-Type: application/json']
+		);
+		json = API.getJSONFromResponse(response);
+		assert200(response);
+		version = json.successful[0].version;
+		let lastModifiedByUser = json.successful[0].meta.lastModifiedByUser;
+		assert.ok(lastModifiedByUser);
+
+		API.useAPIKey(config.get('user1APIKey'));
+
+		// Collection change
+		response = await API.groupPost(
+			groupID,
+			'collections',
+			JSON.stringify([{ name: 'Test Collection' }]),
+			['Content-Type: application/json']
+		);
+		let collectionJSON = API.getJSONFromResponse(response);
+		let collectionKey = collectionJSON.successful[0].key;
+
+		response = await API.groupPost(
+			groupID,
+			'items',
+			JSON.stringify([{
+				key: key,
+				version: version,
+				collections: [collectionKey]
+			}]),
+			['Content-Type: application/json']
+		);
+		assert200(response);
+		json = API.getJSONFromResponse(response);
+		version = json.successful[0].version;
+		assert.deepEqual(
+			lastModifiedByUser,
+			json.successful[0].meta.lastModifiedByUser,
+			'lastModifiedByUser changed after collection change'
+		);
+
+		// Trash
+		response = await API.groupPost(
+			groupID,
+			'items',
+			JSON.stringify([{
+				key: key,
+				version: version,
+				deleted: true
+			}]),
+			['Content-Type: application/json']
+		);
+		assert200(response);
+		json = API.getJSONFromResponse(response);
+		version = json.successful[0].version;
+		assert.deepEqual(
+			lastModifiedByUser,
+			json.successful[0].meta.lastModifiedByUser,
+			'lastModifiedByUser changed after trash'
+		);
+
+		// Untrash
+		response = await API.groupPost(
+			groupID,
+			'items',
+			JSON.stringify([{
+				key: key,
+				version: version,
+				deleted: false
+			}]),
+			['Content-Type: application/json']
+		);
+		assert200(response);
+		json = API.getJSONFromResponse(response);
+		version = json.successful[0].version;
+		assert.deepEqual(
+			lastModifiedByUser,
+			json.successful[0].meta.lastModifiedByUser,
+			'lastModifiedByUser changed after untrash'
+		);
+
+		// Relations
+		response = await API.groupPost(
+			groupID,
+			'items',
+			JSON.stringify([{
+				key: key,
+				version: version,
+				relations: {
+					'dc:relation': 'http://zotero.org/groups/' + groupID + '/items/AAAAAAAA'
+				}
+			}]),
+			['Content-Type: application/json']
+		);
+		assert200(response);
+		json = API.getJSONFromResponse(response);
+		version = json.successful[0].version;
+		assert.deepEqual(
+			lastModifiedByUser,
+			json.successful[0].meta.lastModifiedByUser,
+			'lastModifiedByUser changed after relations change'
+		);
+	});
+
 	// PHP: testNumChildrenJSON
 	it('should return numChildren in JSON', async function () {
 		let json = await API.createItem('book', false, 'json');

commit 5708a602d24e32b155fdd5633fc99fdb2b1a63a2

Fix lastModifiedByUserID not being updated for Zotero client edits
Regression from 2300dd41 ("Only update lastModifiedByUserID when
dateModified changes"), which tied lastModifiedByUserID updates to
skipDateModifiedUpdate. Because the Zotero client always sends
dateModified in sync uploads and its User-Agent matches the dateModified
hack from 4f4bf3f2, skipDateModifiedUpdate is always true for desktop
edits, which inadvertently also suppressed the lastModifiedByUserID
update.

Decouple the two by computing skipLastModifiedByUserIDUpdate based only
on the changed fields (collections, inPublications, relations).

model/Item.inc.php
@@ -1682,10 +1682,10 @@ class Zotero_Item extends Zotero_DataObject {
 				
 				$this->_serverDateModified = $timestamp;
 				
-				// Group item data -- only update lastModifiedByUserID if dateModified
-				// was updated (i.e., not for collection membership, trash, etc.)
+				// Group item data -- skip lastModifiedByUserID update for changes that
+				// don't count as modifying the item (e.g., collection membership, relations)
 				if ($isGroupLibrary && $userID
-						&& empty($options['skipDateModifiedUpdate'])) {
+						&& empty($options['skipLastModifiedByUserIDUpdate'])) {
 					$sql = "INSERT INTO groupItems VALUES (?, ?, ?)
 								ON DUPLICATE KEY UPDATE lastModifiedByUserID=?";
 					Zotero_DB::query($sql, array($this->_id, null, $userID, $userID), $shardID);
@@ -2429,7 +2429,10 @@ class Zotero_Item extends Zotero_DataObject {
 	 */
 	public function updateVersion($userID) {
 		$this->changed['version'] = true;
-		$this->save($userID, ['skipDateModifiedUpdate' => true]);
+		$this->save($userID, [
+			'skipDateModifiedUpdate' => true,
+			'skipLastModifiedByUserIDUpdate' => true
+		]);
 	}
 	
 	

model/Items.inc.php
@@ -1885,20 +1885,23 @@ class Zotero_Items {
 			$item->inPublications = !empty($json->inPublications);
 		}
 		
-		// Skip "Date Modified" update if only certain fields were updated (e.g., collections)
-		$skipDateModifiedUpdate = $dateModifiedProvided || !sizeOf(array_diff(
+		// Skip "Date Modified" and "Modified By" updates if only certain fields were updated
+		// (e.g., collections)
+		$skipLastModifiedByUserIDUpdate = !sizeOf(array_diff(
 			$item->getChanged(),
 			['collections', 'inPublications', 'relations']
 		));
-		
+		$skipDateModifiedUpdate = $dateModifiedProvided || $skipLastModifiedByUserIDUpdate;
+
 		if ($item->hasChanged() && !$skipDateModifiedUpdate
 				&& (($apiVersion >= 3 && $tmpZoteroClientDateModifiedHack) || !$changedDateModified)) {
 			// Update item with the current timestamp
 			$item->dateModified = Zotero_DB::getTransactionTimestamp();
 		}
-		
+
 		$changed = $item->save($userID, [
-			'skipDateModifiedUpdate' => $skipDateModifiedUpdate
+			'skipDateModifiedUpdate' => $skipDateModifiedUpdate,
+			'skipLastModifiedByUserIDUpdate' => $skipLastModifiedByUserIDUpdate
 		]) || $changed;
 		
 		// Additional steps that have to be performed on a saved object

tests/remote/tests/3/item.test.js
@@ -1682,6 +1682,46 @@ describe('Items', function () {
 		);
 	});
 
+	it('should update lastModifiedByUser for Zotero client with dateModified', async function () {
+		let groupID = config.get('ownedPrivateGroupID');
+
+		// Create item as user 1
+		let json = await API.groupCreateItem(groupID, 'book', {}, 'json');
+		let key = json.key;
+		let version = json.version;
+
+		// Edit a field as user 2, emulating a desktop client sync upload:
+		// new dateModified in JSON + Zotero User-Agent
+		API.useAPIKey(config.get('user2APIKey'));
+		let newDateModified = new Date(Date.now() + 1000).toISOString().replace(/\.\d+Z$/, 'Z');
+		let response = await API.groupPost(
+			groupID,
+			'items',
+			JSON.stringify([{
+				key,
+				version,
+				title: 'Modified Title',
+				dateModified: newDateModified
+			}]),
+			[
+				'Content-Type: application/json',
+				'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15) Zotero/9.0'
+			]
+		);
+		assert200(response);
+		json = API.getJSONFromResponse(response);
+		assert.equal(
+			json.successful[0].data.dateModified,
+			newDateModified,
+			'client-supplied dateModified not preserved'
+		);
+		assert.equal(
+			json.successful[0].meta.lastModifiedByUser.id,
+			config.get('userID2'),
+			'lastModifiedByUser not updated for Zotero client edit with dateModified'
+		);
+	});
+
 	it('should update lastModifiedByUser when trashing', async function () {
 		let groupID = config.get('ownedPrivateGroupID');
 